339bf44963785ea3e8cbe10f2cea6bde11c20b83
Amalbek <aralbayamalbek@gmail.com>
2023-06-17 19:24:37 +0600

    Create Tables for DB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Follower;
+use App\Models\Photo;
+use App\Models\User;
+use Database\Factories\UserFollowerFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       $this->call([
+          UserSeeder::class,
+           PhotoSeeder::class,
+           CommentSeeder::class
+       ]);
     }
 }

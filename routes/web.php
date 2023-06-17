d1deff4d49dc105995e635c2a6e02a56bb9f189e
Amalbek <aralbayamalbek@gmail.com>
2023-06-17 19:27:03 +0600

    Make routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::middleware('language')
+    ->group(function () {
+Route::resource('users',\App\Http\Controllers\UsersController::class);
+Route::resource('photos',\App\Http\Controllers\PhotosController::class);
+Route::resource('photos.comments', \App\Http\Controllers\CommentsController::class)->middleware('auth');
+Auth::routes();
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    });
+
+
+Route::get('/language/{locale}', [\App\Http\Controllers\LanguageSwitcherController::class, 'switcher'])
+    ->name('language.switcher')
+    ->where('locale', 'en|ru');
